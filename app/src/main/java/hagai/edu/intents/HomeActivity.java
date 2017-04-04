4d64232660a7bac66c5faa426dd5f750fb577799
hagaizamir <hagai34@gmail.com>
2017-04-04 21:10:30 +0300

    1

--- app/src/main/java/hagai/edu/intents/HomeActivity.java
+++ app/src/main/java/hagai/edu/intents/HomeActivity.java
@@ -1,7 +1,9 @@
 package hagai.edu.intents;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,14 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+//get the opening intent
+        Intent theIntent = getIntent();
+
+        //get the Extras (the data)
+        String userName = theIntent.getStringExtra(MainActivity.EXTRA_USER_NAME);
+
+        Toast.makeText(this, "Hello  "+ userName, Toast.LENGTH_SHORT).show();
+
     }
 }
--- app/src/main/java/hagai/edu/intents/MainActivity.java
+++ app/src/main/java/hagai/edu/intents/MainActivity.java
@@ -5,9 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    public static final String EXTRA_USER_NAME = "theUserName";
+    private EditText etUserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,14 +19,28 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
 
         findViewById(R.id.btnSave).setOnClickListener(this);
+        (etUserName) = (EditText) findViewById(R.id.etUserName);
 
     }
 
     @Override
     public void onClick(View v) {
         //Context
-        Intent homeIntent = new Intent(this , HomeActivity.class);
+//        if ((v.getId()== R.id.btnSave))
+        //built the intent
+
+
+        Intent homeIntent = new Intent(this, HomeActivity.class);
+
         //send some info
+        String userName = etUserName.getText().toString();
+
+        homeIntent.putExtra(EXTRA_USER_NAME, userName);
+        homeIntent.putExtra("theUserScore", 10);
+
+        //Launch intent
         startActivity(homeIntent);
+
+
     }
 }
